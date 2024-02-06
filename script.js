070b212201d91e21fdd8efd5c0ad483e52b13b39
parunkumar645 <parunkumar645@gmail.com>
2024-02-06 15:32:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("Btn");
+var light = document.getElementById("light");
+
+function toggleBtn() {
+  btn.classList.toggle("active");
+  light.classList.toggle("on");
+}
